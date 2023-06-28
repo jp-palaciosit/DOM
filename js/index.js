9ac7255c6c7de00d35a95d82c9455669d3dea54f
Kaiael24 <juanchii10p@gmail.com>
2023-06-28 02:16:50 -0300

    Se agregaron actividades en el DOM

--- js/index.js
+++ js/index.js
@@ -1,12 +1,23 @@
 
 let captureName = document.querySelector(".captureName")
-let name = prompt("Como te llamas? ")
-
-captureName.innerHTML = name
+/* let name = prompt("Como te llamas? ")
+ */
+/* captureName.innerHTML = name */
 
 let button = document.getElementById("buttonCv")
 let body = document.querySelector("body")
 
 button.addEventListener("click", function(e) {
     button.style.background = "Black"
+})
+
+let menuItems = document.querySelectorAll(".menuItems")
+
+menuItems.forEach(function(item){
+    item.addEventListener("click", function(e){
+        const currentItem = document.querySelector(".active")
+        currentItem.classList.remove("active")
+        e.target.classList.add("active")
+    })
+
 })
